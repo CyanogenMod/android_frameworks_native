8e846fd6b3e09f5a52f47311e97b8d8f683833d4
Dave Burke <daveburke@google.com>
2012-04-24 16:06:50 -0700

    Define OMX_AUDIO_AACObjectELD
    Change-Id: Icfbcbddb46fc0bf185e00b193462bc9e035c222b

--- include/media/openmax/OMX_Audio.h
+++ include/media/openmax/OMX_Audio.h
@@ -261,7 +261,8 @@ typedef enum OMX_AUDIO_AACPROFILETYPE{
   OMX_AUDIO_AACObjectERLC = 17,     /**< ER AAC Low Complexity object (Error Resilient AAC-LC) */
   OMX_AUDIO_AACObjectLD = 23,       /**< AAC Low Delay object (Error Resilient) */
   OMX_AUDIO_AACObjectHE_PS = 29,    /**< AAC High Efficiency with Parametric Stereo coding (HE-AAC v2, object type PS) */
-  OMX_AUDIO_AACObjectKhronosExtensions = 0x6F000000, /**< Reserved region for introducing Khronos Standard Extensions */ 
+  OMX_AUDIO_AACObjectELD = 39,      /** AAC Enhanced Low Delay. NOTE: Pending Khronos standardization **/
+  OMX_AUDIO_AACObjectKhronosExtensions = 0x6F000000, /**< Reserved region for introducing Khronos Standard Extensions */
   OMX_AUDIO_AACObjectVendorStartUnused = 0x7F000000, /**< Reserved region for introducing Vendor Extensions */
   OMX_AUDIO_AACObjectMax = 0x7FFFFFFF
 } OMX_AUDIO_AACPROFILETYPE;
@@ -1325,4 +1326,3 @@ typedef struct OMX_AUDIO_CONFIG_NOISEREDUCTIONTYPE {
 
 #endif
 /* File EOF */
-
