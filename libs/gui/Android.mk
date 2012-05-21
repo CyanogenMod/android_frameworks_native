e10824965c64663b8878b808206ee382c7ab6af8
Jesse Hall <jessehall@google.com>
2012-05-21 11:23:28 -0700

    Enable SurfaceTexture fence sync for the emulator
    The host-accelerated GL ES driver doesn't do the implicit
    synchronization required when fence sync isn't used.
    
    Bug: 6515813
    Change-Id: I6a667f2db6b519f3557b5abda78775f767841dae

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -42,6 +42,10 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM), s5pc110)
 	LOCAL_CFLAGS += -DUSE_FENCE_SYNC
 endif
+ifneq ($(filter generic%,$(TARGET_DEVICE)),)
+    # Emulator build
+    LOCAL_CFLAGS += -DUSE_FENCE_SYNC
+endif
 
 ifeq ($(TARGET_BOARD_PLATFORM), tegra)
 	LOCAL_CFLAGS += -DALLOW_DEQUEUE_CURRENT_BUFFER
