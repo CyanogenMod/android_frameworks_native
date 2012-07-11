14f3f9398575313da91b7958cebde759e22b2808
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-11 21:30:42 +0300

    MemoryHeapBase: ifdef for gingerbread/froyo compatibility
    Allow to "revert" by a board config flag the offset member addon
    see commit f24c4cd0f20 (aosp change id Ie618fb5c0718e)
    
    This change is required to use overlay based proprietary camera libs
    coming from gingerbread and froyo builds.
    
    This change also requires a global cflag named BINDER_COMPAT
    because a lot of modules use the MemoryHeap includes
    
    Add this in your BoardConfig.mk :
    COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
    
    Backported from /#/c/13317/
    
    Change-Id: I2e5c10d3cd9584803dd282aa17650661d6499e5b

--- include/binder/MemoryHeapBase.h
+++ include/binder/MemoryHeapBase.h
@@ -61,7 +61,9 @@ public:
     virtual void*       getBase() const;
     virtual size_t      getSize() const;
     virtual uint32_t    getFlags() const;
-    virtual uint32_t      getOffset() const;
+#ifndef BINDER_COMPAT
+    virtual uint32_t    getOffset() const;
+#endif
 
     const char*         getDevice() const;
 
@@ -91,7 +93,9 @@ private:
     uint32_t    mFlags;
     const char* mDevice;
     bool        mNeedUnmap;
+#ifndef BINDER_COMPAT
     uint32_t    mOffset;
+#endif
 };
 
 // ---------------------------------------------------------------------------
