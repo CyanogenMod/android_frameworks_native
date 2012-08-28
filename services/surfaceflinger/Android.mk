89575f02c8abdba25d8bb5c6d104f2d5c5315c7f
Steve Kondik <shade@chemlab.org>
2012-08-27 19:49:49 -0700

    Fix makefile from the previous commit
    Change-Id: Ia245a408cf02ee0d1968bbc05ac181fe1129b9ce

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -31,13 +31,6 @@ ifeq ($(TARGET_BOARD_PLATFORM), s5pc110)
 	LOCAL_CFLAGS += -DNEVER_DEFAULT_TO_ASYNC_MODE
 endif
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE), true)
-    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
-    LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
-    LOCAL_SHARED_LIBRARIES += libqdutils
-    LOCAL_CFLAGS += -DQCOMHW
-endif
-
 ifeq ($(TARGET_DISABLE_TRIPLE_BUFFERING), true)
 	LOCAL_CFLAGS += -DTARGET_DISABLE_TRIPLE_BUFFERING
 endif
@@ -51,6 +44,13 @@ LOCAL_SHARED_LIBRARIES := \
 	libbinder \
 	libui \
 	libgui
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE), true)
+    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
+    LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
+    LOCAL_SHARED_LIBRARIES += libqdutils
+    LOCAL_CFLAGS += -DQCOMHW
+endif
 
 # this is only needed for DDMS debugging
 ifneq ($(TARGET_BUILD_PDK), true)
