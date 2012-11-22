8430095879d2fa6878e68f8f12da4e704815ac09
Mathias Agopian <mathias@google.com>
2012-11-21 16:02:13 -0800

    make transform hint multi-display aware
    if a layer is not mirrored, we now use its display
    as the source for the transfrom hint calculation
    instead of always using the default (main) display.
    
    this change does two thing:
    1) we make updateTransformHint take a DisplayDevice
       as a parameter instead of hard-coding the
       main display.
    
    2) each time we do a transaction that could change
       the hint, we go through all layers and
       figure out which display should be used for their
       transform hint.
    
    Bug: 7599344
    Change-Id: I9b04a95e6c372dd770bacf81d8ef6f8e31b87b83

--- services/surfaceflinger/Layer.h
+++ services/surfaceflinger/Layer.h
@@ -91,8 +91,8 @@ public:
     inline const sp<GraphicBuffer>& getActiveBuffer() const { return mActiveBuffer; }
 
     // Updates the transform hint in our SurfaceTexture to match
-    // the current orientation of the default display device.
-    virtual void updateTransformHint() const;
+    // the current orientation of the display device.
+    virtual void updateTransformHint(const sp<const DisplayDevice>& hw) const;
 
 protected:
     virtual void onFirstRef();
