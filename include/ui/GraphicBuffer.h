71a0fd20b282bb62b09cd16e73ee41286707d008
codeworkx <codeworkx@cyanogenmod.org>
2012-11-24 14:51:44 +0100

    exynos4: ui: define USAGE_HW_FIMC1 to support gralloc
    Source:
    http://git.insignal.co.kr/samsung/exynos/android/platform/frameworks/native/commit/?h=exynos-jb&id=5179fef67cadaa10d72cfe6764629565bc9a1e4e
    
    Change-Id: Id194446eb332869f677dfb7e94c73aa3f52ee4b0

--- include/ui/GraphicBuffer.h
+++ include/ui/GraphicBuffer.h
@@ -63,7 +63,10 @@ public:
         USAGE_HW_2D             = GRALLOC_USAGE_HW_2D,
         USAGE_HW_COMPOSER       = GRALLOC_USAGE_HW_COMPOSER,
         USAGE_HW_VIDEO_ENCODER  = GRALLOC_USAGE_HW_VIDEO_ENCODER,
-        USAGE_HW_MASK           = GRALLOC_USAGE_HW_MASK
+        USAGE_HW_MASK           = GRALLOC_USAGE_HW_MASK,
+#ifdef EXYNOS4_ENHANCEMENTS
+        USAGE_HW_FIMC1          = GRALLOC_USAGE_HW_FIMC1
+#endif
     };
 
     GraphicBuffer();
