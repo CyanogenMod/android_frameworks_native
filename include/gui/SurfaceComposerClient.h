5247f8435efd7155b481939deeb74ad1be7069ba
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-24 22:39:21 +0000

    Revert "Revert "Compatibility work around for bad graphics driver dependency.""
    This reverts commit 38b657265ccc5ae45bd7860a68b0d9373b47a2f3.
    
    Change-Id: If350d73839f1ed5e01af4299d44837e619d14d66

--- include/gui/SurfaceComposerClient.h
+++ include/gui/SurfaceComposerClient.h
@@ -73,6 +73,10 @@ public:
 
     /* triggers screen on and waits for it to complete */
     static void unblankDisplay(const sp<IBinder>& display);
+    // TODO: Remove me.  Do not use.
+    // This is a compatibility shim for one product whose drivers are depending on
+    // this legacy function (when they shouldn't).
+    static status_t getDisplayInfo(int32_t displayId, DisplayInfo* info);
 
     // ------------------------------------------------------------------------
     // surface creation / destruction
