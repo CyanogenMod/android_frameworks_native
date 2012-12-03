dc9fa4ac6c0f102925801234d54a83eec5d227d2
codeworkx <codeworkx@cyanogenmod.org>
2012-12-03 22:16:56 +0100

    libui: exynos4: apply FIMC usage flag on format HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED
    Change-Id: I8a98e593cb60d0843e83a5e431142410e26d82e7

--- libs/ui/GraphicBufferAllocator.cpp
+++ libs/ui/GraphicBufferAllocator.cpp
@@ -122,9 +122,10 @@ status_t GraphicBufferAllocator::alloc(uint32_t w, uint32_t h,
 #else
 
 #ifdef EXYNOS4_ENHANCEMENTS
-    if ((format == 0x101) || (format == 0x105)) {
+    if ((format == 0x101) || (format == 0x105) || (format == 0x107)) {
         // 0x101 = HAL_PIXEL_FORMAT_YCbCr_420_P (Samsung-specific pixel format)
         // 0x105 = HAL_PIXEL_FORMAT_YCbCr_420_SP (Samsung-specific pixel format)
+        // 0x107 = HAL_PIXEL_FORMAT_YCbCr_420_SP_TILED (Samsung-specific pixel format)
         usage |= GRALLOC_USAGE_HW_FIMC1; // Exynos HWC wants FIMC-friendly memory allocation
     }
 #endif
