a6252fbc8ece3cb6267e3d397efce853953434d7
codeworkx <codeworkx@cyanogenmod.org>
2012-12-04 18:51:36 +0100

    binder: Add MemoryHeapBaseIon
    Source:
    http://git.insignal.co.kr/samsung/exynos/android/platform/frameworks/native/commit/?h=exynos-jb&id=dc4cd25cc41e4358debd0c7d1a2706d208a58df6
    
    Change-Id: Ib06cc37a2a25c78a061ee2bad48eec2d01b07833

--- include/binder/IMemory.h
+++ include/binder/IMemory.h
@@ -36,7 +36,8 @@ public:
 
     // flags returned by getFlags()
     enum {
-        READ_ONLY   = 0x00000001
+        READ_ONLY   = 0x00000001,
+        USE_ION_FD  = 0x00000008
     };
 
     virtual int         getHeapID() const = 0;
