af2d946f2de04cd1a53da443e8f9129f7759e335
codeworkx <codeworkx@cyanogenmod.org>
2012-12-04 18:51:36 +0100

    binder: Squashed commit of MemoryHeapBaseIon
    Source:
    http://git.insignal.co.kr/samsung/exynos/android/platform/frameworks/native/commit/?h=exynos-jb&id=dc4cd25cc41e4358debd0c7d1a2706d208a58df6
    
    Change-Id: Ib06cc37a2a25c78a061ee2bad48eec2d01b07833
    
    binder: update MemoryHeapIon
    
    * Update from ODROID-XU 09232013 BSP release
    
    Change-Id: I5245c8a9f783e8902bf91a0ee23e60ebeb335b27
    
    binder: update MemoryHeapIon
    
    * Update from ODROID-XU 04212014 BSP
    
    Change-Id: Ifc2664bcde37a71d855e05e7c9e50288a4508892
    
    binder: Fixed new CM SLSI build variant
    
    Change-Id: Icfff592cf705af660c7318b08fce75dbbf42103c
    (cherry picked from commit 014ad5eee0a7de70c4a9f66e8f5ce7b32f4ecb16)

--- libs/binder/Android.mk
+++ libs/binder/Android.mk
@@ -38,11 +38,31 @@ sources := \
     Static.cpp \
     TextOutput.cpp \
 
+ifeq ($(BOARD_NEEDS_MEMORYHEAPION),true)
+sources += \
+    MemoryHeapIon.cpp
+endif
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+
+ifeq ($(BOARD_NEEDS_MEMORYHEAPION),true)
+LOCAL_SHARED_LIBRARIES += libion_exynos
+LOCAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
+
+ifeq ($(TARGET_SLSI_VARIANT),cm)
+SLSI_DIR := samsung_slsi-cm
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)
+else
+SLSI_DIR := samsung_slsi
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)-$(TARGET_SLSI_VARIANT)
+endif
+LOCAL_C_INCLUDES += hardware/$(SLSI_DIR)/$(PLATFORM_DIR)/include
+endif
+
 LOCAL_MODULE := libbinder
-LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
+LOCAL_SHARED_LIBRARIES += liblog libcutils libutils
 LOCAL_SRC_FILES := $(sources)
 ifneq ($(TARGET_USES_64_BIT_BINDER),true)
 ifneq ($(TARGET_IS_64_BIT),true)
@@ -53,9 +73,30 @@ LOCAL_CFLAGS += -Werror
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+
+ifeq ($(BOARD_NEEDS_MEMORYHEAPION),true)
+LOCAL_SHARED_LIBRARIES += libion_exynos
+LOCAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
+
+ifeq ($(TARGET_SLSI_VARIANT),cm)
+SLSI_DIR := samsung_slsi-cm
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)
+else
+SLSI_DIR := samsung_slsi
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)-$(TARGET_SLSI_VARIANT)
+endif
+LOCAL_C_INCLUDES += hardware/$(SLSI_DIR)/$(PLATFORM_DIR)/include
+endif
+
 LOCAL_MODULE := libbinder
 LOCAL_STATIC_LIBRARIES += libutils
 LOCAL_SRC_FILES := $(sources)
+ifeq ($(BOARD_NEEDS_MEMORYHEAPPMEM),true)
+LOCAL_C_INCLUDES += \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_ADDITIONAL_DEPENDENCIES := \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+endif
 ifneq ($(TARGET_USES_64_BIT_BINDER),true)
 ifneq ($(TARGET_IS_64_BIT),true)
 LOCAL_CFLAGS += -DBINDER_IPC_32BIT=1
--- libs/binder/IMemory.cpp
+++ libs/binder/IMemory.cpp
@@ -32,6 +32,10 @@
 #include <binder/Parcel.h>
 #include <utils/CallStack.h>
 
+#ifdef USE_MEMORY_HEAP_ION
+#include "ion.h"
+#endif
+
 #define VERBOSE   0
 
 namespace android {
@@ -300,6 +304,14 @@ void BpMemoryHeap::assertReallyMapped() const
                 IInterface::asBinder(this).get(),
                 parcel_fd, size, err, strerror(-err));
 
+#ifdef USE_MEMORY_HEAP_ION
+        ion_client ion_client_num = -1;
+        if (flags & USE_ION_FD) {
+            ion_client_num = ion_client_create();
+            ALOGE_IF(ion_client_num < 0, "BpMemoryHeap : ion client creation error");
+        }
+#endif
+
         int fd = dup( parcel_fd );
         ALOGE_IF(fd==-1, "cannot dup fd=%d, size=%zd, err=%d (%s)",
                 parcel_fd, size, err, strerror(errno));
@@ -312,7 +324,16 @@ void BpMemoryHeap::assertReallyMapped() const
         Mutex::Autolock _l(mLock);
         if (mHeapId == -1) {
             mRealHeap = true;
-            mBase = mmap(0, size, access, MAP_SHARED, fd, offset);
+
+#ifdef USE_MEMORY_HEAP_ION
+        if (flags & USE_ION_FD) {
+            if (ion_client_num < 0)
+                mBase = MAP_FAILED;
+            else
+                mBase = ion_map(fd, size, offset);
+            } else
+#endif
+                mBase = mmap(0, size, access, MAP_SHARED, fd, offset);
             if (mBase == MAP_FAILED) {
                 ALOGE("cannot map BpMemoryHeap (binder=%p), size=%zd, fd=%d (%s)",
                         IInterface::asBinder(this).get(), size, fd, strerror(errno));
@@ -324,6 +345,12 @@ void BpMemoryHeap::assertReallyMapped() const
                 android_atomic_write(fd, &mHeapId);
             }
         }
+#ifdef USE_MEMORY_HEAP_ION
+        if (ion_client_num < 0)
+            ion_client_num = -1;
+        else
+            ion_client_destroy(ion_client_num);
+#endif
     }
 }
 
