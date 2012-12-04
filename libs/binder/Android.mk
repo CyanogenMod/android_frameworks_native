014ad5eee0a7de70c4a9f66e8f5ce7b32f4ecb16
codeworkx <codeworkx@cyanogenmod.org>
2012-12-04 18:51:36 +0100

    binder: Squashed commit of MemoryHeapBaseIon
    Source:
    http://git.insignal.co.kr/samsung/exynos/android/platform/frameworks/native/commit/?h=exynos-jb&id=dc4cd25cc41e4358debd0c7d1a2706d208a58df6
    
    Change-Id: Ib06cc37a2a25c78a061ee2bad48eec2d01b07833
    
    binder: update MemoryHeapIon
    
    * Update from ODROID-XU 09232013 BSP release
    
    Change-Id: I5245c8a9f783e8902bf91a0ee23e60ebeb335b27
    
    binder: update MemoryHeapIon
    
    * Update from ODROID-XU 04212014 BSP
    
    Change-Id: Ifc2664bcde37a71d855e05e7c9e50288a4508892
    
    Change-Id: Icfff592cf705af660c7318b08fce75dbbf42103c

--- libs/binder/Android.mk
+++ libs/binder/Android.mk
@@ -41,11 +41,28 @@ sources += \
     MemoryHeapPmem.cpp
 endif
 
+ifeq ($(BOARD_NEEDS_MEMORYHEAPION),true)
+sources += \
+    MemoryHeapIon.cpp
+endif
+
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+
+ifeq ($(BOARD_NEEDS_MEMORYHEAPION),true)
+LOCAL_SHARED_LIBRARIES += libion_exynos
+LOCAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
+ifneq ($(TARGET_SLSI_VARIANT),)
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)-$(TARGET_SLSI_VARIANT)
+else
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)
+endif
+LOCAL_C_INCLUDES += hardware/samsung_slsi/$(PLATFORM_DIR)/include
+endif
+
 LOCAL_MODULE := libbinder
-LOCAL_SHARED_LIBRARIES := liblog libcutils libutils
+LOCAL_SHARED_LIBRARIES += liblog libcutils libutils
 LOCAL_SRC_FILES := $(sources)
 ifeq ($(BOARD_NEEDS_MEMORYHEAPPMEM),true)
 LOCAL_C_INCLUDES += \
@@ -62,6 +79,18 @@ LOCAL_CFLAGS += -Werror
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+
+ifeq ($(BOARD_NEEDS_MEMORYHEAPION),true)
+LOCAL_SHARED_LIBRARIES += libion_exynos
+LOCAL_CFLAGS += -DUSE_MEMORY_HEAP_ION
+ifneq ($(TARGET_SLSI_VARIANT),)
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)-$(TARGET_SLSI_VARIANT)
+else
+PLATFORM_DIR := $(TARGET_BOARD_PLATFORM)
+endif
+LOCAL_C_INCLUDES += hardware/samsung_slsi/$(PLATFORM_DIR)/include
+endif
+
 LOCAL_MODULE := libbinder
 LOCAL_STATIC_LIBRARIES += libutils
 LOCAL_SRC_FILES := $(sources)
