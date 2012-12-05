6aaba7d8949acf9f0adb8502a3e15e7ddbba1246
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-12-05 12:40:45 +0700

    Sensor: Add RotationVectorSensor2
    (ported from 4.1)
    
    Some ICS apps (namely, Google Maps) expects a rotation vector to be
    available. Newer devices, this is provided by either Android's
    sensor fusion (requires Gyro) or by hardware sensor fusion (MPL).
    
    Older devices will lack this virtual sensor and compass in Google
    Maps will not work. To fix this, we can provide our own rotation
    vector sensor by converting the values from the orientation sensor.
    (They are basically the same information in different formats.)
    
    Thanks to Unhelpful for the help with related math.
    
    Change-Id: I39489b3a5ce7c7d890768614357f32cc491bd6d9

--- services/sensorservice/Android.mk
+++ services/sensorservice/Android.mk
@@ -9,6 +9,7 @@ LOCAL_SRC_FILES:= \
     LinearAccelerationSensor.cpp \
     OrientationSensor.cpp \
     RotationVectorSensor.cpp \
+    RotationVectorSensor2.cpp \
     SensorDevice.cpp \
     SensorFusion.cpp \
     SensorInterface.cpp \
