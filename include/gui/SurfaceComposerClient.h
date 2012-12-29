9d08f2dd8347f1f681f7eb2d8069ebd6f992932a
Chirayu Desai <cdesai@cyanogenmod.org>
2012-12-29 12:56:13 +0530

    SurfaceComposerClient: bring back getDisplayWidth, Height & Orientation
    enable it with either ICS_CAMERA_BLOB or MR0_CAMERA_BLOB CFLAG
    
    Change-Id: Ieb5d36ad2a87ed1f0aa28ae3afbd5d48c880d6ab

--- include/gui/SurfaceComposerClient.h
+++ include/gui/SurfaceComposerClient.h
@@ -78,6 +78,12 @@ public:
     // this legacy function (when they shouldn't).
     static status_t getDisplayInfo(int32_t displayId, DisplayInfo* info);
 
+#if defined(ICS_CAMERA_BLOB) || defined(MR0_CAMERA_BLOB)
+    static ssize_t getDisplayWidth(int32_t displayId);
+    static ssize_t getDisplayHeight(int32_t displayId);
+    static ssize_t getDisplayOrientation(int32_t displayId);
+#endif
+
     // ------------------------------------------------------------------------
     // surface creation / destruction
 
