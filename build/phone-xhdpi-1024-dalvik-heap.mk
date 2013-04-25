9856e93970fd6def1349e564f17d42f505904eba
Andrew Bartholomew <andrewb03@gmail.com>
2013-04-25 13:48:21 -0400

    build/phone-xhdpi-1024-dalvik-heap.mk Revert AOSP heapgrowthlimit change from 64 to 96
    This reverts part of AOSP change at
    
    https://android.googlesource.com/platform/frameworks/native/+/c84e9844d621223d14178be521
    
    Possible performance issues have arisen because of it. Discussion at
    
    http://code.google.com/p/android/issues/detail?id=40961
    
    Patch Set 2: Clean up commit message
    
    Change-Id: Id7e3324607f92553a98e4d47da58a322e4fba657

--- build/phone-xhdpi-1024-dalvik-heap.mk
+++ build/phone-xhdpi-1024-dalvik-heap.mk
@@ -18,7 +18,7 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapgrowthlimit=96m \
+    dalvik.vm.heapgrowthlimit=64m \
     dalvik.vm.heapsize=256m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
