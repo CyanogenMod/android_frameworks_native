5cd46aa3399ddeaf12a211390dfde66c796ab299
Dianne Hackborn <hackbod@google.com>
2013-07-09 15:01:40 -0700

    Update to follow procstats options changes.
    Change-Id: I6f016c944cfcd01bbbcf3cf37bc7ff05482f0012

--- cmds/dumpstate/dumpstate.c
+++ cmds/dumpstate/dumpstate.c
@@ -284,8 +284,7 @@ static void dumpstate() {
     run_command("CHECKIN BATTERYSTATS", 30, "dumpsys", "batterystats", "--checkin", NULL);
     run_command("CHECKIN MEMINFO", 30, "dumpsys", "meminfo", "--checkin", NULL);
     run_command("CHECKIN NETSTATS", 30, "dumpsys", "netstats", "--checkin", NULL);
-    run_command("CHECKIN PROCSTATS", 30, "dumpsys", "procstats", "-c",
-            "--include-committed", NULL);
+    run_command("CHECKIN PROCSTATS", 30, "dumpsys", "procstats", "-c", NULL);
     run_command("CHECKIN USAGESTATS", 30, "dumpsys", "usagestats", "--c", NULL);
 
     printf("========================================================\n");
