1bd5068a5180ac12dda374e15c7b1f70e8334284
Dianne Hackborn <hackbod@google.com>
2013-07-11 11:45:18 -0700

    Fix cmd line arguments for usagestats.
    Change-Id: I9a1436aa1d1665924ba62a568bb4b66d67ae2814

--- cmds/dumpstate/dumpstate.c
+++ cmds/dumpstate/dumpstate.c
@@ -285,7 +285,7 @@ static void dumpstate() {
     run_command("CHECKIN MEMINFO", 30, "dumpsys", "meminfo", "--checkin", NULL);
     run_command("CHECKIN NETSTATS", 30, "dumpsys", "netstats", "--checkin", NULL);
     run_command("CHECKIN PROCSTATS", 30, "dumpsys", "procstats", "-c", NULL);
-    run_command("CHECKIN USAGESTATS", 30, "dumpsys", "usagestats", "--c", NULL);
+    run_command("CHECKIN USAGESTATS", 30, "dumpsys", "usagestats", "-c", NULL);
 
     printf("========================================================\n");
     printf("== Running Application Activities\n");
