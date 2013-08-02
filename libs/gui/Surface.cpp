4b49e08385a96398aa2633e096fe916dc9f08768
Andy McFadden <fadden@android.com>
2013-08-02 15:31:45 -0700

    Roll back recent change
    Disable dropping of frames based on timestamp.  Resume auto-
    generating timestamps in Surface.
    
    Bug 10151804
    
    Change-Id: I15de26158e1d7ef22a5b150e685a126dc48ae2b4

--- libs/gui/Surface.cpp
+++ libs/gui/Surface.cpp
@@ -261,7 +261,9 @@ int Surface::queueBuffer(android_native_buffer_t* buffer, int fenceFd) {
     Mutex::Autolock lock(mMutex);
     int64_t timestamp;
     if (mTimestamp == NATIVE_WINDOW_TIMESTAMP_AUTO) {
-        timestamp = 0;
+        timestamp = systemTime(SYSTEM_TIME_MONOTONIC);
+        ALOGV("Surface::queueBuffer making up timestamp: %.2f ms",
+            timestamp / 1000000.f);
     } else {
         timestamp = mTimestamp;
     }
