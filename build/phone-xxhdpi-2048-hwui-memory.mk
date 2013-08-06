386f220e174f9ed5aad487867223033fd5d986c6
Steve Kondik <shade@chemlab.org>
2013-08-06 02:53:19 -0700

    hwui: Update configuration for 2GB/1080p devices
    Change-Id: I73357a35bee22d76d3fe8a98ece91c5c9d19371d

--- build/phone-xxhdpi-2048-hwui-memory.mk
+++ build/phone-xxhdpi-2048-hwui-memory.mk
@@ -19,12 +19,12 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hwui.texture_cache_size=48 \
     ro.hwui.layer_cache_size=32 \
-    ro.hwui.path_cache_size=8 \
-    ro.hwui.shape_cache_size=2 \
+    ro.hwui.r_buffer_cache_size=4 \
+    ro.hwui.path_cache_size=24 \
     ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=4 \
-    ro.hwui.texture_cache_flush_rate=0.5 \
+    ro.hwui.drop_shadow_cache_size=5 \
+    ro.hwui.texture_cache_flushrate=0.5 \
     ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=512 \
+    ro.hwui.text_small_cache_height=1024 \
     ro.hwui.text_large_cache_width=2048 \
     ro.hwui.text_large_cache_height=1024
