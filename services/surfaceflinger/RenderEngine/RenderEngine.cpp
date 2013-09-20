d555684cb36dfb959694db76962e570184f98838
Mathias Agopian <mathias@google.com>
2013-09-19 17:08:37 -0700

    reinstate black-screenshot debugging code
    turned off by default.
    
    Bug: 10809349
    Change-Id: I3e6b8c7860e6b0e122b8f07de4020967cd1f005c

--- services/surfaceflinger/RenderEngine/RenderEngine.cpp
+++ services/surfaceflinger/RenderEngine/RenderEngine.cpp
@@ -201,6 +201,10 @@ void RenderEngine::deleteTextures(size_t count, uint32_t const* names) {
     glDeleteTextures(count, names);
 }
 
+void RenderEngine::readPixels(size_t l, size_t b, size_t w, size_t h, uint32_t* pixels) {
+    glReadPixels(l, b, w, h, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+}
+
 void RenderEngine::dump(String8& result) {
     const GLExtensions& extensions(GLExtensions::getInstance());
     result.appendFormat("GLES: %s, %s, %s\n",
