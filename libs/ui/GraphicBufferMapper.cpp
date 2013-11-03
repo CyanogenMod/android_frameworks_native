46b7e320853f1f5951de1c017d38851b49fd2d02
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-03 03:06:55 +0000

    exynos4: getphys implementation on buffermanager for samsung omx
    Conflicts:
    	include/media/hardware/HardwareAPI.h
    	include/ui/GraphicBufferMapper.h
    	libs/ui/GraphicBufferMapper.cpp
    
    Conflicts:
    	libs/ui/GraphicBufferMapper.cpp
    
    Change-Id: I88db141e29a50986bee8d9555d145d1f92ea6ecf

--- libs/ui/GraphicBufferMapper.cpp
+++ libs/ui/GraphicBufferMapper.cpp
@@ -171,5 +171,19 @@ status_t GraphicBufferMapper::unlockAsync(buffer_handle_t handle, int *fenceFd)
     return err;
 }
 
+#ifdef EXYNOS4_ENHANCEMENTS
+status_t GraphicBufferMapper::getphys(buffer_handle_t handle, void** paddr)
+{
+    status_t err;
+
+    err = mAllocMod->getphys(mAllocMod, handle, paddr);
+
+    ALOGW_IF(err, "getphys(%p) fail %d(%s)",
+    handle, err, strerror(-err));
+    return err;
+}
+#endif
+
+
 // ---------------------------------------------------------------------------
 }; // namespace android
