ce4876a9387a9b9b93b32ca8e736dc67bbeda466
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-03 03:17:16 +0000

    sensors: Add dummy light sensor
    Some devices have a sysfs toggle for the lightsensor instead of
    passing the LUX values for userspace processing; for those,
    use BOARD_SYSFS_LIGHT_SENSOR:=<sysfs path>
    
    This is necessary for the rest of userspace to recognize its existence
    and toggle it for auto-brightness. It doesn't generate actual light
    values, though; all backlight adjustments are made in-kernel.
    
    Change-Id: I0e546b4740720bd34d1e1d85a96b295fcc697106
    
    sensors: dummy ls: set unique handle
    
    handles and types are usually the same, but it's not mandatory.
    Sony DASH use custom handles, and the previous assumption broke
    Proximity sensor on Sony devices.
    
    Change-Id: I954e13765f42341e6b5393a12556f6a007c4e494
    
    Conflicts:
    	services/sensorservice/Android.mk
    	services/sensorservice/SensorDevice.cpp

--- services/sensorservice/Android.mk
+++ services/sensorservice/Android.mk
@@ -29,6 +29,10 @@ LOCAL_SHARED_LIBRARIES := \
 	libui \
 	libgui
 
+ifneq ($(BOARD_SYSFS_LIGHT_SENSOR),)
+    LOCAL_CFLAGS += -DSYSFS_LIGHT_SENSOR=\"$(BOARD_SYSFS_LIGHT_SENSOR)\"
+endif
+
 LOCAL_MODULE:= libsensorservice
 
 include $(BUILD_SHARED_LIBRARY)
