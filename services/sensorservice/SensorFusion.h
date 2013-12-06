ebff73c37d5f1581702430f4a0348c160b99b57e
Aravind Akella <aakella@google.com>
2013-12-05 16:24:59 -0800

    Bug fix for SensorFusion data rate.
    SensorFusion is always returning data at the slowest possible sampling rate (5 Hz). batch() is getting called twice, first time with the requested rate and second time with the slowest rate (which overwrites the requested rate). Fix batch call in SensorFusion::activate()
    
    Bug: 12064319
    Change-Id: If62f3e514233f69810336fd22b136b4395b667d3

--- services/sensorservice/SensorFusion.h
+++ services/sensorservice/SensorFusion.h
@@ -37,7 +37,6 @@ class SensorDevice;
 
 class SensorFusion : public Singleton<SensorFusion> {
     friend class Singleton<SensorFusion>;
-    static const nsecs_t DEFAULT_EVENTS_PERIOD = 200000000;  //  5 Hz
 
     SensorDevice& mSensorDevice;
     Sensor mAcc;
