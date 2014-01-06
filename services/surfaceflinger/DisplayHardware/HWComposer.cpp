ff7096a03ec4db9b28e5a858019374c61d80e04c
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-01-06 01:11:40 +0000

    sf: Fix detection of 0.3 hwcomposer
    The format was different, so look at the header bits too...
    
    Change-Id: Ibc3d4c9f6bca74d6951041f92e2ecc6189e58c41

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -66,7 +66,8 @@ static bool hwcHasApiVersion(const hwc_composer_device_1_t* hwc,
 }
 
 static bool hwcHasVsyncEvent(const hwc_composer_device_1_t* hwc) {
-    return hwcHasApiVersion(hwc, HWC_DEVICE_API_VERSION_0_3);
+    return hwcHasApiVersion(hwc, HWC_DEVICE_API_VERSION_0_3) ||
+           hwcHeaderVersion(hwc) >= 3;
 }
 
 static size_t sizeofHwcLayerList(const hwc_composer_device_1_t* hwc,
