dc832dc5513f0767c153f90a57356c3466f45dd4
Serban Constantinescu <serban.constantinescu@arm.com>
2014-01-30 14:58:32 +0000

    ServiceManager: Fix Android.mk
    This patch fixes Android.mk and enables building bctest as an optional
    module without any extra hacks.
    
    Change-Id: Icaf8bf9452776db2ea4a2ba75f3abf05b4e2cdab
    Signed-off-by: Serban Constantinescu <serban.constantinescu@arm.com>

--- cmds/servicemanager/Android.mk
+++ cmds/servicemanager/Android.mk
@@ -1,12 +1,19 @@
 LOCAL_PATH:= $(call my-dir)
 
-#include $(CLEAR_VARS)
-#LOCAL_SRC_FILES := bctest.c binder.c
-#LOCAL_MODULE := bctest
-#include $(BUILD_EXECUTABLE)
+svc_c_flags =	\
+	-Wall -Wextra \
+
+include $(CLEAR_VARS)
+LOCAL_SHARED_LIBRARIES := liblog
+LOCAL_SRC_FILES := bctest.c binder.c
+LOCAL_CFLAGS += $(svc_c_flags)
+LOCAL_MODULE := bctest
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := service_manager.c binder.c
+LOCAL_CFLAGS += $(svc_c_flags)
 LOCAL_MODULE := servicemanager
 include $(BUILD_EXECUTABLE)
