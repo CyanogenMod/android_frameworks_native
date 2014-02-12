a8dd5ccf72fbe0e7b17fe85d1dd387f7696a3724
Manoj Kumar AVM <manojavm@codeaurora.org>
2014-02-11 23:12:50 -0800

    sf: Determine if virtual display can use HWC for composition
    For HWC virtual display using V4L2 approach:
    1. HWC virtual display implementation doesn't send output buffer
       back to Surface Flinger.
    2. For non-wfd virtual displays(e.g. Simulated Secondary Displays,
       Screen Record, CTS etc..), configure VDS to use GPU for
       composition on output/sink buffer.
    3. For wfd virtual display (a.k.a hybrid wfd), use
       Framebuffer surface (similar to external display mechanism).
    
    For HWC virtual display using VDS approach:
    1. HWC is allocated for first virtual display connection.
    2. Subsequent virtual display sessions will be composed by GLES driver.
    ToDo: Modify VDS component to allocate hwcDisplayId based on
    mForceHwcCopy (which is based on Usage Flags)
    
    Change-Id: Ib7d2c00ca5fa07908b2eac8085654f8d2cd9af0b

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -88,7 +88,8 @@ HWComposer::HWComposer(
       mFbDev(0), mHwc(0), mNumDisplays(1),
       mCBContext(new cb_context),
       mEventHandler(handler),
-      mDebugForceFakeVSync(false)
+      mDebugForceFakeVSync(false),
+      mVDSEnabled(false)
 {
     for (size_t i =0 ; i<MAX_HWC_DISPLAYS ; i++) {
         mLists[i] = 0;
@@ -184,6 +185,15 @@ HWComposer::HWComposer(
         for (size_t i =0 ; i<NUM_BUILTIN_DISPLAYS ; i++) {
             queryDisplayProperties(i);
         }
+    }
+
+    // read system property for VDS solution
+    // This property is expected to be setup once during bootup
+    if( (property_get("persist.hwc.enable_vds", value, NULL) > 0) &&
+        ((!strncmp(value, "1", strlen("1"))) ||
+        !strncasecmp(value, "true", strlen("true")))) {
+        //HAL virtual display is using VDS based implementation
+        mVDSEnabled = true;
     }
 
     if (needVSyncThread) {
