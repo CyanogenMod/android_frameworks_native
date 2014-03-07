37bf0789f5c1167f6f7f5667b09cad70e0bb27d4
Jeff Brown <jeffbrown@google.com>
2014-03-07 13:57:59 -0800

    Declare KEYCODE_SLEEP and KEYCODE_WAKEUP.
    Bug: 12938999
    Change-Id: I544661b3887332998d703b7b2df9e56c23ab48c4

--- include/android/keycodes.h
+++ include/android/keycodes.h
@@ -266,6 +266,8 @@ enum {
     AKEYCODE_BRIGHTNESS_DOWN = 220,
     AKEYCODE_BRIGHTNESS_UP   = 221,
     AKEYCODE_MEDIA_AUDIO_TRACK = 222,
+    AKEYCODE_SLEEP           = 223,
+    AKEYCODE_WAKEUP          = 224,
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
