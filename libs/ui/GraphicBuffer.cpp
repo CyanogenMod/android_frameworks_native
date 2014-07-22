b2aada6e8481575304f50594d715b8eeac1b54be
Colin Cross <ccross@android.com>
2014-07-22 15:55:08 -0700

    crash if getNativeBuffer() called on NULL GraphicBuffer
    If getNativeBuffer() is called on a NULL GraphicBuffer the
    static_cast of this from GraphicBuffer* to ANativeWindowBuffer*
    will return a small pointer like (ANativeWindowBuffer*)0x10.
    This value can propagate past NULL checks until it causes a crash
    far away from the original NULL pointer.  Crash immediately
    instead.
    
    Change-Id: Id614b9eb1484108b3c3c733545309844c4b87532

--- libs/ui/GraphicBuffer.cpp
+++ libs/ui/GraphicBuffer.cpp
@@ -126,6 +126,7 @@ void GraphicBuffer::dumpAllocationsToSystemLog()
 
 ANativeWindowBuffer* GraphicBuffer::getNativeBuffer() const
 {
+    LOG_ALWAYS_FATAL_IF(this == NULL, "getNativeBuffer() called on NULL GraphicBuffer");
     return static_cast<ANativeWindowBuffer*>(
             const_cast<GraphicBuffer*>(this));
 }
