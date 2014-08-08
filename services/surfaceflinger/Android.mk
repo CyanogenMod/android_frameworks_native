28bcbbba8cee8088a47a5698a325747485da00d4
Raj Kamal <rkamal@codeaurora.org>
2014-08-08 09:42:22 +0530

    sf: Enable QCOM_BSP if TARGET_USES_QCOM_BSP is set.
    Enable QCOM_BSP if TARGET_USES_QCOM_BSP is set in target
    BoardConfig
    
    Change-Id: I0288976c2d2000eff62430ad3d726725bda3e135

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -96,6 +96,13 @@ LOCAL_SHARED_LIBRARIES := \
 	libgui \
 	libpowermanager
 
+ifeq ($(TARGET_USES_QCOM_BSP), true)
+    LOCAL_C_INCLUDES += hardware/qcom/display/libgralloc
+    LOCAL_C_INCLUDES += hardware/qcom/display/libqdutils
+    LOCAL_SHARED_LIBRARIES += libqdutils
+    LOCAL_CFLAGS += -DQCOM_BSP
+endif
+
 LOCAL_MODULE:= libsurfaceflinger
 
 include $(BUILD_SHARED_LIBRARY)
