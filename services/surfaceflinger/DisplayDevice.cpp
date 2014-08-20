f2568dea419562ad75b2e79dcbd358af34e9f8ad
Michael Lentine <mlentine@google.com>
2014-08-20 10:51:23 -0700

    Destroy eglSurface before recreating.
    Bug: 16856208
    
    Change-Id: Ifb5c8997afab4bc922356a3542d1f899a2546855

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -401,6 +401,11 @@ status_t DisplayDevice::orientationToTransfrom(
 void DisplayDevice::setDisplaySize(const int newWidth, const int newHeight) {
     dirtyRegion.set(getBounds());
 
+    if (mSurface != EGL_NO_SURFACE) {
+        eglDestroySurface(mDisplay, mSurface);
+        mSurface = EGL_NO_SURFACE;
+    }
+
     mDisplaySurface->resizeBuffers(newWidth, newHeight);
 
     ANativeWindow* const window = mNativeWindow.get();
