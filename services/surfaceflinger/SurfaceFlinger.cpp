ebdaf884e92db4201183c93ea593b765c6914dcd
Saurabh Shah <saurshah@codeaurora.org>
2014-08-21 16:09:32 -0700

    SF: Check ViewFrame validity before sending to hwc
    Check if ViewFrame is valid before sending to hwc to make sure
    composition produces valid values.
    
    A ViewFrame can get invalid values when the framework is disconnected
    which is a step necessary for some testing scenarios.
    
    Change-Id: I2ad76061b6664dbb7d2aa3d66acaf99b880aedfe

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -1539,7 +1539,8 @@ void SurfaceFlinger::handleTransactionLocked(uint32_t transactionFlags)
 #ifdef QCOM_B_FAMILY
                             // Set the view frame of each display only of its
                             // default orientation.
-                            if(orient == DisplayState::eOrientationDefault) {
+                            if(orient == DisplayState::eOrientationDefault and
+                                    state.frame.isValid()) {
                                 qdutils::setViewFrame(disp->getHwcDisplayId(),
                                     state.frame.left, state.frame.top,
                                     state.frame.right, state.frame.bottom);
