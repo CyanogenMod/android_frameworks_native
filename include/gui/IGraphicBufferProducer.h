af2dde6228c618e24f88d3a037a5a1e547cd08f5
Diogo Ferreira <defer@cyngn.com>
2014-10-10 14:03:35 +0100

    native: Make setting the buffers size conditional to qualcomm hardware
    This is a partial ifdef of CAF commit d6eecf50919312c1dd778c5742de72bb98f98f1a.
    
    Some devices might have proprietary bits that link against libgui/libui components
    and are not expecting the ABI mismatch introduced in that commit.
    
    Change-Id: I0670990fcb903ee750d4d2925175b9588cf35f45

--- include/gui/IGraphicBufferProducer.h
+++ include/gui/IGraphicBufferProducer.h
@@ -238,6 +238,7 @@ public:
     // connected to the specified client API.
     virtual status_t disconnect(int api) = 0;
 
+#ifdef QCOM_HARDWARE
     // setBufferSize enables to specify the user defined size of the buffer
     // that needs to be allocated by surfaceflinger for its client. This is
     // useful for cases where the client doesn't want the gralloc to calculate
@@ -245,6 +246,7 @@ public:
     // calculate the size for the buffer. this will take effect from next
     // dequeue buffer.
     virtual status_t setBuffersSize(int size) = 0;
+#endif
 
 };
 
