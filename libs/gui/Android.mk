704f342e7ad5b50d4fead6656827549db9157149
Ethan Chen <intervigil@gmail.com>
2014-11-18 14:16:17 -0800

    frameworks/native: Use direct project pathmap
    Change-Id: I0abbab64ca034944eec6878c5d0ad655e1aee38e

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -48,7 +48,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 # Executed only on QCOM BSPs
 ifeq ($(TARGET_USES_QCOM_BSP),true)
-    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/$(TARGET_BOARD_PLATFORM)/libgralloc
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
     LOCAL_CFLAGS += -DQCOM_BSP
 endif
 
