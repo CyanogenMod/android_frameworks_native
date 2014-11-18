8dbc9d82f76567e0c981eff22d604ed438e99b9a
Ethan Chen <intervigil@gmail.com>
2014-11-18 14:16:17 -0800

    frameworks/native: Use direct project pathmap
    Change-Id: I0abbab64ca034944eec6878c5d0ad655e1aee38e

--- services/surfaceflinger/Android.mk
+++ services/surfaceflinger/Android.mk
@@ -97,8 +97,8 @@ LOCAL_SHARED_LIBRARIES := \
 	libpowermanager
 
 ifeq ($(TARGET_USES_QCOM_BSP), true)
-    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/$(TARGET_BOARD_PLATFORM)/libgralloc
-    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/$(TARGET_BOARD_PLATFORM)/libqdutils
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+    LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libqdutils
     LOCAL_SHARED_LIBRARIES += libqdutils
     LOCAL_CFLAGS += -DQCOM_BSP
 endif
