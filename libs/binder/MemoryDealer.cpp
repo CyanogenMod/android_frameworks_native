a40cfcf4ef68686fea6a4d13e422a0496420a644
Thomas Wendt <thoemy@gmx.net>
2014-11-24 22:00:03 +0100

    libbinder: Add legacy MemoryDealer constructor for old Nvidia OMX adaptor
    Fixes the following error:
    
    E/hwcomposer(  158): failed to load /system/lib/libnvcap_video.so: dlopen failed: could not load library "libnvomxadaptor.so" needed by "libnvcap_video.so"; caused by cannot locate symbol "_ZN7android12MemoryDealerC1EjPKc" referenced by "libnvomxadaptor.so"
    
    Change-Id: Ib9b96898875fbe6b508bbcf170575919c9728b82

--- libs/binder/MemoryDealer.cpp
+++ libs/binder/MemoryDealer.cpp
@@ -225,6 +225,13 @@ Allocation::~Allocation()
 
 // ----------------------------------------------------------------------------
 
+#ifdef ADD_LEGACY_MEMORY_DEALER_CONSTRUCTOR_SYMBOL
+extern "C" void _ZN7android12MemoryDealerC2EjPKcj(void* obj, size_t size, const char* name, uint32_t flags);
+extern "C" void _ZN7android12MemoryDealerC1EjPKc(void* obj, size_t size, const char* name) {
+    _ZN7android12MemoryDealerC2EjPKcj(obj, size, name, 0);
+}
+#endif
+
 MemoryDealer::MemoryDealer(size_t size, const char* name, uint32_t flags)
     : mHeap(new MemoryHeapBase(size, flags, name)),
     mAllocator(new SimpleBestFitAllocator(size))
