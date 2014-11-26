81c6d66a1daf740109eaaa10953a592907087e96
Duane Sand <duane.sand@imgtec.com>
2014-11-25 16:43:13 -0800

    [MIPS] Build libGLES for mips64, mips32r6
    For now, use generic C code instead of assembler.
    
    Change-Id: I604ecdcde2d5373f1f68ab97a63a80667b630846

--- opengl/libagl/Android.mk
+++ opengl/libagl/Android.mk
@@ -31,7 +31,9 @@ LOCAL_SHARED_LIBRARIES := libcutils libhardware libutils liblog libpixelflinger 
 LOCAL_SRC_FILES_arm += fixed_asm.S iterators.S
 LOCAL_CFLAGS_arm += -fstrict-aliasing
 
+ifndef ARCH_MIPS_REV6
 LOCAL_SRC_FILES_mips += arch-mips/fixed_asm.S
+endif
 LOCAL_CFLAGS_mips += -fstrict-aliasing
 # The graphics code can generate division by zero
 LOCAL_CFLAGS_mips += -mno-check-zero-division
