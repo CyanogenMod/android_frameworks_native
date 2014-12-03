474c1d5731c0fa73a15bcecaadec3c5b5cd981be
Steve Kondik <steve@cyngn.com>
2014-12-03 10:54:59 -0800

    Revert "omx: Add nBitsPerSample parameter for FLAC"
    This reverts commit 5fa201c9919d22b3470ae64e4952df2221ba5b16.
    
    Change-Id: I800c9c057de4d42d19bf95ff07fa382616dfff6d

--- include/media/openmax/OMX_Audio.h
+++ include/media/openmax/OMX_Audio.h
@@ -357,7 +357,6 @@ typedef struct OMX_AUDIO_PARAM_FLACTYPE {
                                    unknown sampling rate. */
     OMX_U32 nCompressionLevel;/**< FLAC compression level, from 0 (fastest compression)
                                    to 8 (highest compression */
-    OMX_U32 nBitsPerSample;   /**< Number of bits in each sample */
 } OMX_AUDIO_PARAM_FLACTYPE;
 
 
