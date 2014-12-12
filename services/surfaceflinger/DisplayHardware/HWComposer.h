f9890356928f6dbae9a4a88418c170cceaf9d351
Thomas Wendt <thoemy@gmx.net>
2014-12-12 15:38:36 +0100

    surfaceflinger: Add more ifdefs for secure display query
    Commit f04661 forgot ifdefs in HWComposer.h that break ABI. This
    broke at least grouper and endeavoru surfaceflinger.
    
    Change-Id: I9a1bd4e1fb9a2ad5ac0a39e86ef8131b52526e32

--- services/surfaceflinger/DisplayHardware/HWComposer.h
+++ services/surfaceflinger/DisplayHardware/HWComposer.h
@@ -276,7 +276,9 @@ public:
         uint32_t height;
         float xdpi;
         float ydpi;
+#ifdef QCOM_BSP
         bool secure;
+#endif
         nsecs_t refresh;
     };
 
@@ -286,7 +288,9 @@ public:
     sp<Fence> getDisplayFence(int disp) const;
     uint32_t getFormat(int disp) const;
     bool isConnected(int disp) const;
+#ifdef QCOM_BSP
     bool isSecure(int disp) const;
+#endif
 
     // These return the values for the current config of a given display index.
     // To get the values for all configs, use getConfigs below.
