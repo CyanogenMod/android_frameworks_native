c682fa83e3da5971b23ecca838f3cf2755fa6d79
Ricardo Cerqueira <ricardo@cyngn.com>
2014-12-23 23:54:41 +0000

    binder: Don't include the compat symbols on arm64 binaries
    ... Since they're not the same when built at 64-bit, and the likelihood
    of blobs requiring this are slim to none, just remove them
    
    Change-Id: Ib4207055a9aadeb589a92b97a000f94cae270aa3

--- libs/binder/MemoryBase.cpp
+++ libs/binder/MemoryBase.cpp
@@ -45,8 +45,10 @@ MemoryBase::~MemoryBase()
 // ---------------------------------------------------------------------------
 }; // namespace android
 
+#ifndef __LP64__
 // Backwards compatibility for libdatabase_sqlcipher (http://b/8253769).
 extern "C" void _ZN7android10MemoryBaseC1ERKNS_2spINS_11IMemoryHeapEEEij(void*, void*, ssize_t, size_t);
 extern "C" void _ZN7android10MemoryBaseC1ERKNS_2spINS_11IMemoryHeapEEElj(void* obj, void* h, long o, unsigned int size) {
     _ZN7android10MemoryBaseC1ERKNS_2spINS_11IMemoryHeapEEEij(obj, h, o, size);
 }
+#endif
