0b4a916726801924732fff77194c0000f1861840
Naseer Ahmed <naseer@codeaurora.org>
2015-01-07 15:17:42 -0500

    frameworks: egl: Enable loading of SW gles libs
    Based on the property persist.sys.force_sw_gles, ensure that the
    S/W OpenGL library is loaded.
    
    CRs-Fixed: 1021870
    Change-Id: If37bfa979d021d28586e3bf9c1f9c5ce77d3c3a8

--- opengl/libs/EGL/Loader.cpp
+++ opengl/libs/EGL/Loader.cpp
@@ -86,7 +86,12 @@ checkGlesEmulationStatus(void)
     char  prop[PROPERTY_VALUE_MAX];
     int   result = -1;
 
-    /* First, check for qemu=1 */
+    /* First check if forced from HW */
+    property_get("persist.sys.force_sw_gles", prop, "0");
+    if (atoi(prop) == 1)
+        return 0;
+
+    /* Check for qemu=1 */
     property_get("ro.kernel.qemu",prop,"0");
     if (atoi(prop) != 1)
         return -1;
@@ -176,6 +181,14 @@ static void* load_wrapper(const char* path) {
 
 static void setEmulatorGlesValue(void) {
     char prop[PROPERTY_VALUE_MAX];
+
+    property_get("persist.sys.force_sw_gles", prop, "0");
+    if (atoi(prop) == 1) {
+        ALOGD("setEmulatorGlesValue: Force S/W GLES");
+        property_set("qemu.gles", "0");
+        return;
+    }
+
     property_get("ro.kernel.qemu", prop, "0");
     if (atoi(prop) != 1) return;
 
