471ec6b4284f645b5e933537fda88de867105fb2
Dan Stoza <stoza@google.com>
2015-01-21 17:36:06 -0800

    libui: Add Rect(uint32_t, uint32_t)
    Adds a Rect constructor that takes uint32_t instead of int32_t, as
    required by a change to Region and -Werror
    
    Change-Id: If91915e5b4ec9ce9e7ba0fb84f03b045d000b023

--- include/ui/Rect.h
+++ include/ui/Rect.h
@@ -18,6 +18,7 @@
 #define ANDROID_UI_RECT
 
 #include <utils/Flattenable.h>
+#include <utils/Log.h>
 #include <utils/TypeHelpers.h>
 #include <ui/Point.h>
 
@@ -38,6 +39,22 @@ public:
     }
 
     inline Rect(int32_t w, int32_t h) {
+        left = top = 0;
+        right = w;
+        bottom = h;
+    }
+
+    inline Rect(uint32_t w, uint32_t h) {
+        if (w > INT32_MAX) {
+            ALOG(LOG_WARN, "Rect",
+                    "Width %u too large for Rect class, clamping", w);
+            w = INT32_MAX;
+        }
+        if (h > INT32_MAX) {
+            ALOG(LOG_WARN, "Rect",
+                    "Height %u too large for Rect class, clamping", h);
+            h = INT32_MAX;
+        }
         left = top = 0;
         right = w;
         bottom = h;
