6a26be7c2b1e5a84b5d2105780148016889285e6
Lajos Molnar <lajos@google.com>
2015-01-22 16:37:37 -0800

    CpuConsumer: lock buffers that could be YUV as ycbcr
    Bug: 17906609
    Change-Id: Ic71af69ec3b19ab1224ed3ad5e0a97c60e81cda6

--- include/gui/CpuConsumer.h
+++ include/gui/CpuConsumer.h
@@ -54,8 +54,12 @@ class CpuConsumer : public ConsumerBase
         uint32_t    scalingMode;
         int64_t     timestamp;
         uint64_t    frameNumber;
-        // Values below are only valid when using
-        // HAL_PIXEL_FORMAT_YCbCr_420_888, in which case LockedBuffer::data
+        // this is the same as format, except for formats that are compatible with
+        // a flexible format (e.g. HAL_PIXEL_FORMAT_YCbCr_420_888). In the latter
+        // case this contains that flexible format
+        PixelFormat flexFormat;
+        // Values below are only valid when using HAL_PIXEL_FORMAT_YCbCr_420_888
+        // or compatible format, in which case LockedBuffer::data
         // contains the Y channel, and stride is the Y channel stride. For other
         // formats, these will all be 0.
         uint8_t    *dataCb;
