c03e4a3438d1a29e38aeae6749ab9d23489bebb3
Arne Coucheron <arco68@gmail.com>
2015-02-01 07:17:00 +0100

    surfaceflinger: Fix the "Fix the opaque check"
     * QCOM_BSP_LEGACY should use the second choice.
    
    Change-Id: I993fb08f648eab70ae215ee39864bc5914c01123

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -530,7 +530,7 @@ void Layer::setGeometry(
 
     // this gives us only the "orientation" component of the transform
     const State& s(getDrawingState());
-#ifdef QCOM_BSP
+#if defined(QCOM_BSP) && !defined(QCOM_BSP_LEGACY)
     if (!isOpaque(s))
 #else
     if (!isOpaque(s) || s.alpha != 0xFF)
