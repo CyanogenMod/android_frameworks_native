9196bebe4889c9e4a88fea821ee90950b56ed8bd
nadlabak <pavel@doshaska.net>
2015-04-17 00:55:53 +0200

    Support WAKE flag in keyboard layouts
    This flag is used to specify the internal keyboard keys allowed
    to wake the device.
    
    Change-Id: Ic15aca1134206c9b006a4d97dded10bccae640d3

--- include/input/InputEventLabels.h
+++ include/input/InputEventLabels.h
@@ -394,6 +394,7 @@ static const InputEventLabel LEDS[] = {
 };
 
 static const InputEventLabel FLAGS[] = {
+    DEFINE_FLAG(WAKE),
     DEFINE_FLAG(VIRTUAL),
     DEFINE_FLAG(FUNCTION),
     DEFINE_FLAG(GESTURE),
