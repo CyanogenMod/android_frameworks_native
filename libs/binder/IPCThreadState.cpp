a3206e690bcfe5c982a0d7fff7c845b83a3ed91f
Wale Ogunwale <ogunwale@google.com>
2015-04-21 12:29:50 -0700

    Fixed build error with clang/llvm.
    Change-Id: Ideacdc974ebad542df724464ccba9fcfb2b7ea91

--- libs/binder/IPCThreadState.cpp
+++ libs/binder/IPCThreadState.cpp
@@ -403,8 +403,9 @@ void IPCThreadState::blockUntilThreadAvailable()
 {
     pthread_mutex_lock(&mProcess->mThreadCountLock);
     while (mProcess->mExecutingThreadsCount >= mProcess->mMaxThreads) {
-        ALOGW("Waiting for thread to be free. mExecutingThreadsCount=%i mMaxThreads=%i\n",
-                mProcess->mExecutingThreadsCount, mProcess->mMaxThreads);
+        ALOGW("Waiting for thread to be free. mExecutingThreadsCount=%lu mMaxThreads=%lu\n",
+                static_cast<unsigned long>(mProcess->mExecutingThreadsCount),
+                static_cast<unsigned long>(mProcess->mMaxThreads));
         pthread_cond_wait(&mProcess->mThreadCountDecrement, &mProcess->mThreadCountLock);
     }
     pthread_mutex_unlock(&mProcess->mThreadCountLock);
