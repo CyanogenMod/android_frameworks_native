9e415d719e4ceb7a3e93c0d60f9c3bf9ee4fc97f
Anthony Hugh <ahugh@google.com>
2015-06-23 10:49:05 -0700

    DO NOT MERGE: Introduce stem keycodes for Wear
    Cherry-pick from master
    
    This change adds four new stem keycodes for Android Wear. These
    keycodes are intended to represent the various hardware buttons
    around the watch.  There is one primary stem key that will be used
    for power/settings and three generic stem keys that will be
    customizable.
    
    BUG: 21903503
    Change-Id: I762b8c0bcf3780a1756f4c1b8bbf07a039ba7781

--- include/android/keycodes.h
+++ include/android/keycodes.h
@@ -302,7 +302,16 @@ enum {
     AKEYCODE_TV_CONTENTS_MENU = 256,
     AKEYCODE_TV_MEDIA_CONTEXT_MENU = 257,
     AKEYCODE_TV_TIMER_PROGRAMMING = 258,
-    AKEYCODE_HELP            = 259
+    AKEYCODE_HELP            = 259,
+    /** Primary stem key for Wear
+     * Main power/reset button on watch. */
+    AKEYCODE_STEM_PRIMARY = 264,
+    /** Generic stem key 1 for Wear */
+    AKEYCODE_STEM_1 = 265,
+    /** Generic stem key 2 for Wear */
+    AKEYCODE_STEM_2 = 266,
+    /** Generic stem key 3 for Wear */
+    AKEYCODE_STEM_3 = 267
 
     // NOTE: If you add a new keycode here you must also add it to several other files.
     //       Refer to frameworks/base/core/java/android/view/KeyEvent.java for the full list.
