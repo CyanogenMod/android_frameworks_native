01e111b5a02440c0684494e9286709285e20097f
Todd Kennedy <toddke@google.com>
2015-07-31 14:36:20 -0700

    Add a trace constant for PackageManager
    Change-Id: Icd9afc86c88296919e4750d71cb0aef314194bee

--- cmds/atrace/atrace.cpp
+++ cmds/atrace/atrace.cpp
@@ -91,6 +91,7 @@ static const TracingCategory k_categories[] = {
     { "rs",         "RenderScript",     ATRACE_TAG_RS, { } },
     { "bionic",     "Bionic C Library", ATRACE_TAG_BIONIC, { } },
     { "power",      "Power Management", ATRACE_TAG_POWER, { } },
+    { "pm",         "Package Manager",  ATRACE_TAG_PACKAGE_MANAGER, { } },
     { "sched",      "CPU Scheduling",   0, {
         { REQ,      "/sys/kernel/debug/tracing/events/sched/sched_switch/enable" },
         { REQ,      "/sys/kernel/debug/tracing/events/sched/sched_wakeup/enable" },
