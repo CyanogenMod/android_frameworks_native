c784dfc39fa1471b0f653206970ac9a256269256
Praveen Chavan <pchavan@codeaurora.org>
2015-09-16 11:20:00 -0700

    libgui: assign handle to NULL after free
    to avoid use-after-free situations
    
    Change-Id: If9c09f509bc55795856302e5ca34470df019c622

--- libs/ui/GraphicBuffer.cpp
+++ libs/ui/GraphicBuffer.cpp
@@ -112,6 +112,7 @@ void GraphicBuffer::free_handle()
         GraphicBufferAllocator& allocator(GraphicBufferAllocator::get());
         allocator.free(handle);
     }
+    handle = NULL;
     mWrappedBuffer = 0;
 }
 
