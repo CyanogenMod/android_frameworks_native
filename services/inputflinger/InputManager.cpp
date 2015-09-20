f12bd0c9008e5e49397ffefa77fdcc28e23392d5
Steve Kondik <steve@cyngn.com>
2015-09-20 04:20:21 -0700

    input: Adjust priority
     * Bring into the desired group to get the best result.
    
    Change-Id: I3bd031074cd7006994736b4c22d0294b6012f662

--- services/inputflinger/InputManager.cpp
+++ services/inputflinger/InputManager.cpp
@@ -51,13 +51,13 @@ void InputManager::initialize() {
 }
 
 status_t InputManager::start() {
-    status_t result = mDispatcherThread->run("InputDispatcher", PRIORITY_URGENT_DISPLAY);
+    status_t result = mDispatcherThread->run("InputDispatcher", PRIORITY_URGENT_DISPLAY - 1);
     if (result) {
         ALOGE("Could not start InputDispatcher thread due to error %d.", result);
         return result;
     }
 
-    result = mReaderThread->run("InputReader", PRIORITY_URGENT_DISPLAY);
+    result = mReaderThread->run("InputReader", PRIORITY_URGENT_DISPLAY - 1);
     if (result) {
         ALOGE("Could not start InputReader thread due to error %d.", result);
 
