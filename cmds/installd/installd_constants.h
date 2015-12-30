b0fad6d04ba05fbad5ce31c7382be174ed5a70b1
David Brazdil <dbrazdil@google.com>
2015-12-30 11:16:04 +0000

    installd: Rename USEJIT to EXTRACTONLY
    Change-Id: I8d68e32dd9bbb7f6eee2f3aad886792d85cb12ca

--- cmds/installd/installd_constants.h
+++ cmds/installd/installd_constants.h
@@ -75,7 +75,7 @@ constexpr int DEXOPT_PUBLIC       = 1 << 1;
 constexpr int DEXOPT_SAFEMODE     = 1 << 2;
 constexpr int DEXOPT_DEBUGGABLE   = 1 << 3;
 constexpr int DEXOPT_BOOTCOMPLETE = 1 << 4;
-constexpr int DEXOPT_USEJIT       = 1 << 5;
+constexpr int DEXOPT_EXTRACTONLY  = 1 << 5;
 
 /* all known values for dexopt flags */
 constexpr int DEXOPT_MASK =
@@ -83,7 +83,7 @@ constexpr int DEXOPT_MASK =
     | DEXOPT_SAFEMODE
     | DEXOPT_DEBUGGABLE
     | DEXOPT_BOOTCOMPLETE
-    | DEXOPT_USEJIT;
+    | DEXOPT_EXTRACTONLY;
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(*(a)))
 
