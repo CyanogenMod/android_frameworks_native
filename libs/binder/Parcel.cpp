b454d8ffa928e33dd54c979a747994acbd117397
Mark Salyzyn <salyzyn@google.com>
2016-01-27 08:02:48 -0800

    Parcel: file descriptor leak
    Resolve a file descriptor leak when a request for
    ashmem size adjustment is not filed.
    
    Change-Id: I4ebccfd096ec5313725fd99dc3e025f9561d061f

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -170,15 +170,15 @@ static void release_object(const sp<ProcessState>& proc,
             return;
         }
         case BINDER_TYPE_FD: {
-            if (outAshmemSize != NULL) {
-                if (obj.cookie != 0) {
+            if (obj.cookie != 0) { // owned
+                if (outAshmemSize != NULL) {
                     int size = ashmem_get_size_region(obj.handle);
                     if (size > 0) {
                         *outAshmemSize -= size;
                     }
-
-                    close(obj.handle);
                 }
+
+                close(obj.handle);
             }
             return;
         }
