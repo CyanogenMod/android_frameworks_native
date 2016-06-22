41a3853cea0cffede422fc2692b9c8e1674fc5eb
Tim Murray <timmurray@google.com>
2016-06-22 12:42:10 -0700

    surfaceflinger: make critical threads SCHED_FIFO
    Sets the main thread, EventThread, and SFEventThread to SCHED_FIFO to
    minimize jitter.
    
    bug 24503801
    
    Change-Id: I3751c00c79482842f5c0bdaec92b6cbfe62328a0

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -459,6 +459,18 @@ void SurfaceFlinger::init() {
     mSFEventThread = new EventThread(sfVsyncSrc, *this);
     mEventQueue.setEventThread(mSFEventThread);
 
+    // set EventThread and SFEventThread to SCHED_FIFO for minimum jitter
+    struct sched_param param = {0};
+    param.sched_priority = 1;
+    if (sched_setscheduler(mEventThread->getTid(), SCHED_FIFO, &param) != 0) {
+        ALOGE("Couldn't set SCHED_FIFO for EventThread");
+    }
+
+    if (sched_setscheduler(mSFEventThread->getTid(), SCHED_FIFO, &param) != 0) {
+        ALOGE("Couldn't set SCHED_FIFO for SFEventThread");
+    }
+
+
     // Initialize the H/W composer object.  There may or may not be an
     // actual hardware composer underneath.
     mHwc = new HWComposer(this,
