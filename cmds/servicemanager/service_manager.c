f369622da40651da6ac7c01bb646d6309ab35e20
Arve Hjønnevåg <arve@android.com>
2016-08-01 16:05:17 -0700

    ServiceManager: Restore basic uid check
    Prevent apps from registering services without relying on selinux checks.
    
    Bug: 29431260
    
    Change-Id: I38c6e8bc7f7cba1cbd3568e8fed1ae7ac2054a9b
    (cherry picked from commit 2b74d2c1d2a2c1bb6e9c420f7e9b339ba2a95179)

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -121,6 +121,11 @@ static bool check_mac_perms_from_lookup(pid_t spid, uid_t uid, const char *perm,
 static int svc_can_register(const uint16_t *name, size_t name_len, pid_t spid, uid_t uid)
 {
     const char *perm = "add";
+
+    if (uid >= AID_APP) {
+        return 0; /* Don't allow apps to register services */
+    }
+
     return check_mac_perms_from_lookup(spid, uid, perm, str8(name, name_len)) ? 1 : 0;
 }
 
