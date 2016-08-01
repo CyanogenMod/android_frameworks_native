eb304f0a369aa0573a6999057b0b53e22c5dce81
Arve Hjønnevåg <arve@android.com>
2016-08-01 16:05:17 -0700

    DO NOT MERGE ServiceManager: Restore basic uid check
    Prevent apps from registering services without relying on selinux checks.
    
    Bug: 29431260
    
    Change-Id: I38c6e8bc7f7cba1cbd3568e8fed1ae7ac2054a9b

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -107,9 +107,14 @@ static bool check_mac_perms_from_lookup(pid_t spid, const char *perm, const char
     return allowed;
 }
 
-static int svc_can_register(const uint16_t *name, size_t name_len, pid_t spid)
+static int svc_can_register(const uint16_t *name, size_t name_len, pid_t spid, uid_t uid)
 {
     const char *perm = "add";
+
+    if (uid >= AID_APP) {
+        return 0; /* Don't allow apps to register services */
+    }
+
     return check_mac_perms_from_lookup(spid, perm, str8(name, name_len)) ? 1 : 0;
 }
 
@@ -206,7 +211,7 @@ int do_add_service(struct binder_state *bs,
     if (!handle || (len == 0) || (len > 127))
         return -1;
 
-    if (!svc_can_register(s, len, spid)) {
+    if (!svc_can_register(s, len, spid, uid)) {
         ALOGE("add_service('%s',%x) uid=%d - PERMISSION DENIED\n",
              str8(s, len), handle, uid);
         return -1;
