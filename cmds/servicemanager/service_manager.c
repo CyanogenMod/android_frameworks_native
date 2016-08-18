6b19f00360e6b0bd9f98d539dc3c7d770a7c1338
Arve Hjønnevåg <arve@android.com>
2016-08-18 15:42:35 -0700

    ServiceManager: Allow system services running as secondary users to add services
    This should be reverted when all system services have been cleaned up to not
    do this. A process looking up a service while running in the background will
    see the service registered by the active user (assuming the service is
    registered on every user switch), not the service registered by the user that
    the process itself belongs to.
    
    BUG: 30795333
    Change-Id: I1b74d58be38ed358f43c163692f9e704f8f31dbe
    (cherry picked from commit e6bbe69ba739c8a08837134437aaccfea5f1d943)

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <cutils/multiuser.h>
+
 #include <private/android_filesystem_config.h>
 
 #include <selinux/android.h>
@@ -122,7 +124,7 @@ static int svc_can_register(const uint16_t *name, size_t name_len, pid_t spid, u
 {
     const char *perm = "add";
 
-    if (uid >= AID_APP) {
+    if (multiuser_get_app_id(uid) >= AID_APP) {
         return 0; /* Don't allow apps to register services */
     }
 
