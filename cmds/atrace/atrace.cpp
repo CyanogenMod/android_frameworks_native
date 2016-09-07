0f97c1d775dd95fab5142d7aae6d34b80b4c2312
Felipe Leme <felipeal@google.com>
2016-09-07 11:33:26 -0700

    Added a NETWORK trace tag.
    BUG: 31281543
    
    Change-Id: Idc5759eec12f2704c9b9cc48db181f2e669ccc32

--- cmds/atrace/atrace.cpp
+++ cmds/atrace/atrace.cpp
@@ -102,6 +102,7 @@ static const TracingCategory k_categories[] = {
     { "pm",         "Package Manager",  ATRACE_TAG_PACKAGE_MANAGER, { } },
     { "ss",         "System Server",    ATRACE_TAG_SYSTEM_SERVER, { } },
     { "database",   "Database",         ATRACE_TAG_DATABASE, { } },
+    { "network",    "Network",          ATRACE_TAG_NETWORK, { } },
     { k_coreServiceCategory, "Core services", 0, { } },
     { "sched",      "CPU Scheduling",   0, {
         { REQ,      "/sys/kernel/debug/tracing/events/sched/sched_switch/enable" },
