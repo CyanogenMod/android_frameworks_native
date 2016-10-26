12f58910e54084339ae794af7652d4b0dc3818b0
Steve Kondik <steve@cyngn.com>
2016-10-26 15:05:22 -0700

    surfaceflinger: Fix crash when setting priority
     * If phase offsets aren't set, SFEventThread will be null. Fixit.
    
    Change-Id: Ieb4917a2dea4ed5e214215a5ad192d9cf3b72072

--- services/surfaceflinger/SurfaceFlinger_hwc1.cpp
+++ services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -486,12 +486,13 @@ void SurfaceFlinger::init() {
     }
 
     // set SFEventThread to SCHED_FIFO to minimize jitter
-    struct sched_param param = {0};
-    param.sched_priority = 1;
-    if (sched_setscheduler(mSFEventThread->getTid(), SCHED_FIFO, &param) != 0) {
-        ALOGE("Couldn't set SCHED_FIFO for SFEventThread");
+    if (mSFEventThread != NULL) {
+        struct sched_param param = {0};
+        param.sched_priority = 1;
+        if (sched_setscheduler(mSFEventThread->getTid(), SCHED_FIFO, &param) != 0) {
+            ALOGE("Couldn't set SCHED_FIFO for SFEventThread");
+        }
     }
-
 
     // Initialize the H/W composer object.  There may or may not be an
     // actual hardware composer underneath.
