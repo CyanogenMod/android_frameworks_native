3a14886a489d33547958eb6c99803da318228579
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-12-11 20:33:22 +0800

    surfaceflinger: Orient blur layer based on panel mount flip
    As of CAF commit 1818975cab79c0700f30a26a8cccdcc2b424efd5,
    surfaceflinger has support for rotated panel mounts.
    
    Use the appropriate method to orient the blur layer based on it.
    
    Commit 066aa86037bd393b0cb45b93f76c1531cafaebe4 reverted a piece
    of code similar to this, since it was using the outdated methods
    which broke compilation. Bring the code back, this time using
    the appropriate method.
    
    This partially reverts commit 066aa86037bd393b0cb45b93f76c1531cafaebe4.
    
    Change-Id: I2a0272bb7fadef006f22290d21817f041d37883c

--- services/surfaceflinger/LayerBlur.cpp
+++ services/surfaceflinger/LayerBlur.cpp
@@ -228,6 +228,7 @@ bool LayerBlur::captureScreen(const sp<const DisplayDevice>& hw, FBO& fbo, Textu
             texture.getTextureName(), 0);
 
     mFlinger->getRenderEngine().clearWithColor(0.0f, 0.0f, 0.0f, 1.0f);
+    rotation = (Transform::orientation_flags)(rotation ^ hw->getPanelMountFlip());
     mFlinger->renderScreenImplLocked(
                 hw,
                 Rect(0,0,width,height),
